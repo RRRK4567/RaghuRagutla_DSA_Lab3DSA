c5fc7ee5ae34d1c25263ac12fa53d2bb50484f29
RRRK4567 <110348435+RRRK4567@users.noreply.github.com>
2022-11-07 15:55:16 +0530

    Add files via upload

---
+++ BSTPairFinder/src/com/gl/javafsd/dsa/binarysearchtree/pairfinder/BinarySearchTreePairFinderTest.java
@@ -0,0 +1,23 @@
+package com.gl.javafsd.dsa.binarysearchtree.pairfinder;
+
+public class BinarySearchTreePairFinderTest {
+
+	public static void main(String[] args) {
+
+		pairFinderTest(130);
+
+	}
+
+	static void pairFinderTest(int sum) {
+
+		BinarySearchTree sampleTree = BinarySearchTreeTest.sampleBinaryTree1();
+
+		BinarySearchTreeTest test = new BinarySearchTreeTest();
+		BinarySearchTree sampleTree2 = test.sampleBinaryTree1();
+
+		BinarySearchTreePairFinder finder = new BinarySearchTreePairFinder(sampleTree2);
+
+		finder.findPair(sum);
+		System.out.println("-----------------------------------------------------");
+	}
+}
